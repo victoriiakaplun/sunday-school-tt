12c149c2fbd7308ac15fc0bd109e268f301439e5
viktoriia_kaplun <1983vivee>
2020-04-04 22:09:04 +0600

    add admin root handler

--- project/backend/src/controllers/adminController.js
+++ project/backend/src/controllers/adminController.js
@@ -2,17 +2,17 @@ const { User } = require('../db/models');
 const bcrypt = require('bcrypt');
 
 async function insertAdmin() {
-    const admin = User.findOne({ where: { email: 'admin' } });
-    if (!admin) {
-        const SALT_ROUNDS = 10;
-        const salt = bcrypt.genSaltSync(SALT_ROUNDS);
-        await User.create({
-            role: 'admin',
-            name: 'admin',
-            email: 'admin@gmail.com',
-            password: bcrypt.hashSync('admin', salt),
-        });
-    }
+  const admin = User.findOne({ where: { email: 'admin' } });
+  if (!admin) {
+    const SALT_ROUNDS = 10;
+    const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+    await User.create({
+      role: 'admin',
+      name: 'admin',
+      email: 'admin@gmail.com',
+      password: bcrypt.hashSync('admin', salt),
+    });
+  }
 }
 
 module.exports = { insertAdmin };
--- project/backend/src/middleware/adminRootHandler.js
+++ project/backend/src/middleware/adminRootHandler.js
@@ -2,13 +2,14 @@
 const HttpStatus = require('http-status-codes');
 
 async function adminRootCheck(ctx, next) {
-    if (ctx.state.user.role === 'admin') {
-        await next();
-    } else {
-        ctx.response.status = HttpStatus.BAD_REQUEST;
-        ctx.response.body = 'No admin root';
-        return ctx.response;
-    }
+
+  if (ctx.state.user.role === 'admin') {
+    await next();
+  } else {
+    ctx.response.status = HttpStatus.BAD_REQUEST;
+    ctx.response.body = 'No admin root';
+    return ctx.response;
+  }
 }
 
 module.exports = { adminRootCheck };
